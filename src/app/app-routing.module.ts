414ef3372d44adcd79dc2012b47f4686217e07ae
Tin Trempetic <ttrempetic@symmetria-consulting.hr>
2022-11-15 18:09:36 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './containers/post-list/post-list.component';
+import { PostComponent } from './containers/post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: 'posts', pathMatch: 'full', component: PostComponent },
+  { path: 'post/{:id}', pathMatch: 'full', component: PostListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './containers/post/post.component';
+import { PostListComponent } from './containers/post-list/post-list.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    PostListComponent,
+    LayoutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
